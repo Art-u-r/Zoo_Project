78899a3abe1aef53d8b3d84c449ad1c55339845c
Arthur <madoyan_2017@bk.ru>
2023-05-18 14:07:12 +0300

    galleryFirstStage

--- src/components/pages/AnimalPage.jsx
+++ src/components/pages/AnimalPage.jsx
@@ -3,7 +3,7 @@ import OneAnimalPage from './OneAnimalPage'
 
 export default function AnimalPage({ animals }) {
   return (
-    <div> 
+    <div style={{ display:'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop:'60px'}}> 
       {animals.map((e) => <OneAnimalPage key={e.id} animal = {e}/> )}
   </div>
   )
--- src/components/pages/OneAnimalPage.jsx
+++ src/components/pages/OneAnimalPage.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function OneAnimalPage({ animal }) {
-  console.log(animal);
+  const [modal, setMaodal] = useState(false);
+const submitHandler = () => {
+  setMaodal(true)
+}
   return (
-      <div className="card" style={{ width: '18rem' }}>
-        <img src={animal.mainImg} className="card-img-top" alt={animal.animalname} />
+      <div className="card" style={{ width: '18rem', margin:'5px'}}>
+        <img src={animal.mainImg} style={{cursor: 'pointer'}} className="card-img-top" alt={animal.animalname} onClick={submitHandler}/>
         <div className="card-body">
           <p className="card-text">{animal.animalname}</p>
         </div>
+        {
+        (modal)}
       </div>
   );
 }
+
+
